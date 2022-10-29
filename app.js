c1b4eb7110ffffd68e0fcac38d17f38889620671
s9001848 <yenwu@ucdavis.edu>
2022-10-29 11:15:52 -0700

    add login post route

--- app.js
+++ app.js
@@ -80,6 +80,35 @@ app.post('/signup', async (req, res) => {
   }
 })
 
+// Log in to the Database
+app.post('/login', async (req, res) => {
+  const client = new MongoClient(uri)
+  const {email, password} = req.body
+
+  try {
+      await client.connect()
+      const database = client.db('app-data')
+      const users = database.collection('users')
+
+      const user = await users.findOne({email})
+
+      const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+      if (user && correctPassword) {
+          const token = jwt.sign(user, email, {
+              expiresIn: 60 * 24
+          })
+          res.status(201).json({token, userId: user.user_id})
+      }
+
+      res.status(400).json('Invalid Credentials')
+
+  } catch (err) {
+      console.log(err)
+  } finally {
+      await client.close()
+  }
+})
 
 // Server
 const server = app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
