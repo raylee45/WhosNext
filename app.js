36083e1466f87885d3243523ca210dfeac250c99
s9001848 <yenwu@ucdavis.edu>
2022-10-29 11:10:50 -0700

    add signup post route

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 require('./config/passport')(passport);
+const {MongoClient} = require('mongodb')
+const {v4: uuidv4} = require('uuid')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 // App Set up
 const app = express();
+const uri = process.env.URI
 const PORT = process.env.PORT || 8000;
 
 // Middleware
@@ -34,8 +39,47 @@ app.get('/', (req, res) => {
   res.json({ name: 'MERN Auth API', greeting: 'Welcome to the our API', author: 'YOU', message: "Smile, you are being watched by the Backend Engineering Team" });
 });
 
-app.use('/examples', require('./controllers/example'));
-app.use('/users', require('./controllers/user'));
+// Sign up to the Database
+app.post('/signup', async (req, res) => {
+  const client = new MongoClient(uri)
+  const {email, password} = req.body
+
+  const generatedUserId = uuidv4()
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  try {
+      await client.connect()
+      const database = client.db('app-data')
+      const users = database.collection('users')
+
+      const existingUser = await users.findOne({email})
+
+      if (existingUser) {
+          return res.status(409).send('User already exists. Please login')
+      }
+
+      const sanitizedEmail = email.toLowerCase()
+
+      const data = {
+          user_id: generatedUserId,
+          email: sanitizedEmail,
+          hashed_password: hashedPassword
+      }
+
+      const insertedUser = await users.insertOne(data)
+
+      const token = jwt.sign(insertedUser, sanitizedEmail, {
+          expiresIn: 60 * 24
+      })
+      res.status(201).json({token, userId: generatedUserId})
+
+  } catch (err) {
+      console.log(err)
+  } finally {
+      await client.close()
+  }
+})
+
 
 // Server
 const server = app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
