305f788efc2ee059f1e8fd0b121b9436afbb36a4
s9001848 <yenwu@ucdavis.edu>
2022-11-05 14:46:14 -0700

    added match model and controller and put route

--- controllers/matches.js
+++ controllers/matches.js
@@ -9,16 +9,42 @@ const mongoose = require('mongoose');
 
 const Messages = require('../models/message');
 const User = require('../models/user')
+const Match = require('../models/match')
 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
     console.log('=====> Inside GET /matches')
-    User.findById(req.params.id)
-    .then(foundUsers => {
-        res.json({ user: foundUsers });
+    try {
+        User.findAll(req.params.id)
+    }
+    .then((matched)=> {
+        User.
     })
-    .catch(err => {
-        console.log('Error in user#index:', err);
-        res.json({ message: 'Error occured... Please try again.'})
-    });
+
 });
 
+router.delete('/', (req, res) => {
+    Match.findByIdAndRemove(req.params.id)
+    .then(response => {
+        console.log('Match was deleted', response);
+        res.json({ message: `Match ${req.params.id} was deleted`});
+    })
+    .catch(error => {
+        console.log('error', error) 
+        res.json({ message: "Error ocurred, please try again" });
+    })
+});
+
+
+//As a user i must be able to signup and login process
+//As a user i must be able to show homepage the ilst of all user 
+//As a user imust be able to choose one of them as my match
+//As a user I must be able to edit that match
+//As a user I must be able to delete the user
+//Search bar to show name
+//list of user with a match button in front of each user
+
+//user put data into the matches array
+
+//after user login see the list users already see the database (no match)
+//each user name add a button in the front name that says match
+//press the match button goes to the list of matches array 
